c1322c457d6822d31bd42353a0d7c5c518e49f3b
Terence <linterence4@gmail.com>
2022-10-17 15:39:22 -0700

    uploaded low entropy packet

--- client.c
+++ client.c
@@ -6,11 +6,14 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <netinet/in.h>
+#include <fcntl.h>
 struct packet {
 	int length;
 	char bytes[1000];
-}
+};
+
 int main(){
+	int packet_length = 1000;
 	//Create the socket
 	//
 	int network_socket;
@@ -24,24 +27,53 @@ int main(){
 	server_address.sin_port = htons(8765);
 	server_address.sin_addr.s_addr = INADDR_ANY;
 
-	for(int i =0;i<600;i++){
-		sendto()
+	int val = IP_PMTUDISC_DONT;
+	setsockopt(network_socket,IPPROTO_IP, IP_MTU_DISCOVER,&val,sizeof(val));
+
+	//initialize the packet train.
+	struct packet low_train[6000];
+
+	for(int j = 0;j<6000;j++){
+		for (int k = 0;k<packet_length;k++){
+			low_train[j].bytes[k] = 0;
+		}
+		low_train[j].length = packet_length;
 	}
+
+	for(int i =0;i<6000;i++){
+		if(sendto(network_socket,low_train[i].bytes,low_train[i].length,0,(const struct sockaddr*)&server_address,sizeof(server_address))<0){
+			perror("error");
+		}
+	}
+	printf("packets sent!\n");
+	int randomData = open("/dev/urandom", O_RDONLY);
+	if (randomData < 0)
+	{
+		printf("FUCK\n");
+	}
+	else
+	{
+    	char myRandomData[50];
+    	ssize_t result = read(randomData, myRandomData, sizeof myRandomData);
+   	 if (result < 0)
+    	{
+			printf("shit");
+    	}
+
+		printf("%c\n",myRandomData);
+}
 
 	// int connection_status = connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address));
 
 	//Check for error in the connection
-	if(connection_status == -1){
-		printf("Bro your connection sucks");
-	}
 	// receive data from the server
-	char server_response[256];
+	// char server_response[256];
 	//TODO: Create a packet with low entropy data: all 0s
 	//TODO: Create packets with high entropy data: randome sequence of bits.
 	// recv(network_socket, &server_response,sizeof(server_response),0);
 
 	//print out the server's response
-	printf("The server successfully sent the data: %s\n", server_response);
+	// printf("The server successfully sent the data: %s\n", server_response);
 
 	//close the socket
 	close(network_socket);
--- server.c
+++ server.c
@@ -1,17 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#include <sys/socket.h>
-#include <sys/types.h>
-
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
 #include <netinet/in.h>
 
-struct packet {
-	int length;
-	char bytes[256];
-}
 int main(){
-	
+	char bytes;
 	char server_message[256] = "balls";
 
 	//server socket created.
@@ -19,22 +16,25 @@ int main(){
 	server_socket = socket(AF_INET, SOCK_DGRAM, 0);
 
 	//define server address
-	struct sockaddr_in server_address;
+	struct sockaddr_in server_address, client_address;
+
+	memset(&server_address,0,sizeof(server_address));
+	memset(&client_address,0, sizeof(client_address));
+
 	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(9002);
+	server_address.sin_port = htons(8765);
 	server_address.sin_addr.s_addr=INADDR_ANY;
 
 	//bind the socket to the port and ip
-	bind(server_socket,(struct sockaddr*) &server_address, sizeof(server_address));
+	if(bind(server_socket,(struct sockaddr*) &server_address, sizeof(server_address))<0){
+		printf("Binding failed");
+	};
 
-	listen(server_socket, 5);
-
-	int client_socket;
-	client_socket = accept(server_socket, NULL,NULL);
-
-	//send the message
-	send(client_socket, server_message, sizeof(server_message),0);
-
+	int len, n;
+	len = sizeof(client_address);
+	recvfrom(server_socket, (char *)bytes,sizeof(bytes),MSG_WAITALL,(struct sockaddr*) &client_address,len);
+	sendto(server_socket,(const char* )server_message,strlen(server_message),MSG_CONFIRM, (struct sockaddr*) &client_address,len);
+	printf("server received!");
 	//close the socket
 	close(server_socket);
 	return 0;
