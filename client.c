c0c9a04ceef5b6e49d28e878a48b9b98035db540
Terence <linterence4@gmail.com>
2022-10-19 13:06:10 -0700

    created TCP connections

--- client.c
+++ client.c
@@ -7,12 +7,30 @@
 #include <unistd.h>
 #include <netinet/in.h>
 #include <fcntl.h>
+
 struct packet {
 	int length;
 	char bytes[1000];
 };
 
 int main(){
+	//TODO create UDP connection with the server to send config file stuff.
+	int tcp_socket;
+
+	char port_num[256] = "8080";
+	tcp_socket = socket(AF_INET,SOC_STREAM,0);
+
+	struct sockaddr_in probe_address;
+	probe_address.sin_family = AF_INET;
+	probe_address.sin_port = htons(8080);
+	probe_address.sin_addr.s_addr = INADDR_ANY;
+
+	if(connect(tcp_socket,(struct sockaddr*)&probe_address,sizeof(probe_address))<0){
+		printf("cannot connect.");
+	}
+
+
+
 	int packet_length = 1000;
 	//Create the socket
 	//
@@ -31,44 +49,47 @@ int main(){
 	setsockopt(network_socket,IPPROTO_IP, IP_MTU_DISCOVER,&val,sizeof(val));
 
 	//initialize the packet train.
-	struct packet low_train[6000];
+	struct packet *low_train = (struct packet*)malloc(6000 * sizeof(struct packet));
+	struct packet *high_train = (struct packet*)malloc(6000 * sizeof(struct packet));
 
-	for(int j = 0;j<6000;j++){
-		for (int k = 0;k<packet_length;k++){
-			low_train[j].bytes[k] = 0;
-		}
-		low_train[j].length = packet_length;
-	}
-
-	for(int i =0;i<6000;i++){
-		if(sendto(network_socket,low_train[i].bytes,low_train[i].length,0,(const struct sockaddr*)&server_address,sizeof(server_address))<0){
-			perror("error");
-		}
-	}
-	printf("packets sent!\n");
 	int randomData = open("/dev/urandom", O_RDONLY);
-	if (randomData < 0)
-	{
-		printf("FUCK\n");
-	}
-	else
-	{
-    	char myRandomData[50];
-    	ssize_t result = read(randomData, myRandomData, sizeof myRandomData);
-   	 if (result < 0)
-    	{
-			printf("shit");
-    	}
+	unsigned char myRandomData[6000];
 
-		printf("%c\n",myRandomData);
-}
+
+	//initialized the train to be low entropy
+	for(int j = 0;j<6000;j++){
+		high_train[j].length = packet_length;
+		low_train[j].length = packet_length;
+		for (int k = 0;k<packet_length;k++){
+			high_train[j].bytes[k] = myRandomData[k];
+			low_train[j].length = packet_length;
+		}
+		
+	}
+
+	//start to send the low and high entropy data
+	for(int b = 0;b<6000;b++){
+		if(sendto(network_socket,low_train[b].bytes,low_train[b].length,0,(const struct sockaddr*)&server_address,sizeof(server_address))<0){
+			perror("error");
+		}else{
+			printf("low packet sent!\n");
+		}
+	}
+	for(int i =0;i<6000;i++){
+		if(sendto(network_socket,high_train[i].bytes,high_train[i].length,0,(const struct sockaddr*)&server_address,sizeof(server_address))<0){
+			perror("error");
+		}else{
+			printf("high packet sent!\n");
+		}
+	}
+
+	printf("packets sent!\n");
+	//Free the array of packets
+	free(low_train);
+	free(high_train);
 
 	// int connection_status = connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address));
 
-	//Check for error in the connection
-	// receive data from the server
-	// char server_response[256];
-	//TODO: Create a packet with low entropy data: all 0s
 	//TODO: Create packets with high entropy data: randome sequence of bits.
 	// recv(network_socket, &server_response,sizeof(server_response),0);
 
--- server.c
+++ server.c
@@ -9,8 +9,20 @@
 
 int main(){
 	char bytes;
+	int opt = 1;
 	char server_message[256] = "balls";
 
+	int probing_socket;
+	probing_socket = socket(AF_INET, SOCK_STREAM,0);
+
+	if(setsockopt(probing_socket, SOL_SOCKET,SO_REUSEADDR | SO_REUSEPORT, &opt,sizeof(opt))){
+		perror("setsockopt");
+		exit(EXIT_FAILURE);
+	}
+
+	probing_socket.sin_family = AF_INET;
+	probing_socket.sin_family = htons(8080);
+	probing_socket.sin_addr.s_addr = INADDR_ANY;
 	//server socket created.
 	int server_socket;
 	server_socket = socket(AF_INET, SOCK_DGRAM, 0);
@@ -32,12 +44,12 @@ int main(){
 
 	int len, n;
 	len = sizeof(client_address);
-	recvfrom(server_socket, (char *)bytes,sizeof(bytes),MSG_WAITALL,(struct sockaddr*) &client_address,len);
-	sendto(server_socket,(const char* )server_message,strlen(server_message),MSG_CONFIRM, (struct sockaddr*) &client_address,len);
-	printf("server received!");
+	recvfrom(server_socket,bytes,sizeof(bytes),MSG_WAITALL,(struct sockaddr*) &client_address,len);
+	printf("server received!:");
 	//close the socket
 	close(server_socket);
 	return 0;
+
 }
 
 
