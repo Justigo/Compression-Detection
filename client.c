081c0e58a1758f8518b3cba94fc10467d00915ab
Terence <linterence4@gmail.com>
2022-10-19 17:55:49 -0700

    created TCP connection.

--- client.c
+++ client.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -13,90 +14,100 @@ struct packet {
 	char bytes[1000];
 };
 
-int main(){
-	//TODO create UDP connection with the server to send config file stuff.
+
+
+int main(int argc, char *argv[]){
+	// //TODO create UDP connection with the server to send config file stuff.
 	int tcp_socket;
 
 	char port_num[256] = "8080";
-	tcp_socket = socket(AF_INET,SOC_STREAM,0);
-
 	struct sockaddr_in probe_address;
+	tcp_socket = socket(AF_INET,SOCK_STREAM,0);
+
 	probe_address.sin_family = AF_INET;
 	probe_address.sin_port = htons(8080);
 	probe_address.sin_addr.s_addr = INADDR_ANY;
 
+
 	if(connect(tcp_socket,(struct sockaddr*)&probe_address,sizeof(probe_address))<0){
 		printf("cannot connect.");
+		return -1;
 	}
 
+	if(send(tcp_socket,port_num,strlen(port_num),0)<0){
+		printf("cant send message\n");
+		return -1;
+	}
+	close(tcp_socket);
 
 
-	int packet_length = 1000;
-	//Create the socket
-	//
-	int network_socket;
-	//Sock stream =TCP
-	//zero equals default protocol
-	network_socket = socket(AF_INET,SOCK_DGRAM, 0); 
+
+	// int packet_length = 1000;
+	// //Create the socket
+	// //
+	// int network_socket;
+	// //Sock stream =TCP
+	// //zero equals default protocol
+	// network_socket = socket(AF_INET,SOCK_DGRAM, 0); 
 	
-	// give the address for the socket
-	struct sockaddr_in server_address;
-	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(8765);
-	server_address.sin_addr.s_addr = INADDR_ANY;
+	// // give the address for the socket
+	// struct sockaddr_in server_address;
+	// server_address.sin_family = AF_INET;
+	// server_address.sin_port = htons(8765);
+	// server_address.sin_addr.s_addr = INADDR_ANY;
 
-	int val = IP_PMTUDISC_DONT;
-	setsockopt(network_socket,IPPROTO_IP, IP_MTU_DISCOVER,&val,sizeof(val));
+	// int val = IP_PMTUDISC_DONT;
+	// setsockopt(network_socket,IPPROTO_IP, IP_MTU_DISCOVER,&val,sizeof(val));
 
-	//initialize the packet train.
-	struct packet *low_train = (struct packet*)malloc(6000 * sizeof(struct packet));
-	struct packet *high_train = (struct packet*)malloc(6000 * sizeof(struct packet));
+	// //initialize the packet train.
+	// struct packet *low_train = (struct packet*)malloc(6000 * sizeof(struct packet));
+	// struct packet *high_train = (struct packet*)malloc(6000 * sizeof(struct packet));
 
-	int randomData = open("/dev/urandom", O_RDONLY);
-	unsigned char myRandomData[6000];
+	// int randomData = open("/dev/urandom", O_RDONLY);
+	// unsigned char myRandomData[6000];
 
 
-	//initialized the train to be low entropy
-	for(int j = 0;j<6000;j++){
-		high_train[j].length = packet_length;
-		low_train[j].length = packet_length;
-		for (int k = 0;k<packet_length;k++){
-			high_train[j].bytes[k] = myRandomData[k];
-			low_train[j].length = packet_length;
-		}
+	// //initialized the train to be low entropy
+	// for(int j = 0;j<6000;j++){
+	// 	high_train[j].length = packet_length;
+	// 	low_train[j].length = packet_length;
+	// 	for (int k = 0;k<packet_length;k++){
+	// 		high_train[j].bytes[k] = myRandomData[k];
+	// 		low_train[j].length = packet_length;
+	// 	}
 		
-	}
+	// }
 
-	//start to send the low and high entropy data
-	for(int b = 0;b<6000;b++){
-		if(sendto(network_socket,low_train[b].bytes,low_train[b].length,0,(const struct sockaddr*)&server_address,sizeof(server_address))<0){
-			perror("error");
-		}else{
-			printf("low packet sent!\n");
-		}
-	}
-	for(int i =0;i<6000;i++){
-		if(sendto(network_socket,high_train[i].bytes,high_train[i].length,0,(const struct sockaddr*)&server_address,sizeof(server_address))<0){
-			perror("error");
-		}else{
-			printf("high packet sent!\n");
-		}
-	}
+	// //start to send the low and high entropy data
+	// for(int b = 0;b<6000;b++){
+	// 	if(sendto(network_socket,low_train[b].bytes,low_train[b].length,0,(const struct sockaddr*)&server_address,sizeof(server_address))<0){
+	// 		perror("error");
+	// 	}else{
+	// 		printf("low packet sent!\n");
+	// 	}
+	// }
+	// for(int i =0;i<6000;i++){
+	// 	if(sendto(network_socket,high_train[i].bytes,high_train[i].length,0,(const struct sockaddr*)&server_address,sizeof(server_address))<0){
+	// 		perror("error");
+	// 	}else{
+	// 		printf("high packet sent!\n");
+	// 	}
+	// }
 
-	printf("packets sent!\n");
-	//Free the array of packets
-	free(low_train);
-	free(high_train);
+	// printf("packets sent!\n");
+	// //Free the array of packets
+	// free(low_train);
+	// free(high_train);
 
-	// int connection_status = connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+	// // int connection_status = connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address));
 
-	//TODO: Create packets with high entropy data: randome sequence of bits.
-	// recv(network_socket, &server_response,sizeof(server_response),0);
+	// //TODO: Create packets with high entropy data: randome sequence of bits.
+	// // recv(network_socket, &server_response,sizeof(server_response),0);
 
-	//print out the server's response
-	// printf("The server successfully sent the data: %s\n", server_response);
+	// //print out the server's response
+	// // printf("The server successfully sent the data: %s\n", server_response);
 
-	//close the socket
-	close(network_socket);
+	// //close the socket
+	// close(network_socket);
 	return 0;
 }
--- server.c
+++ server.c
@@ -9,45 +9,71 @@
 
 int main(){
 	char bytes;
-	int opt = 1;
-	char server_message[256] = "balls";
 
-	int probing_socket;
+	char port_num[256];
+	struct sockaddr_in probing_address,client_probing_address;
+	memset(port_num, '\0',sizeof(port_num));
+
+	//create the server socket for pre probing phase
+	int probing_socket,client_probe,client_size;
 	probing_socket = socket(AF_INET, SOCK_STREAM,0);
 
-	if(setsockopt(probing_socket, SOL_SOCKET,SO_REUSEADDR | SO_REUSEPORT, &opt,sizeof(opt))){
-		perror("setsockopt");
-		exit(EXIT_FAILURE);
+	probing_address.sin_family = AF_INET;
+	probing_address.sin_family = htons(8080);
+	probing_address.sin_addr.s_addr = INADDR_ANY;
+	
+	if(bind(probing_socket, (struct sockaddr*)&probing_address,sizeof(probing_address))<0){
+		printf("could not bind to port.");
 	}
 
-	probing_socket.sin_family = AF_INET;
-	probing_socket.sin_family = htons(8080);
-	probing_socket.sin_addr.s_addr = INADDR_ANY;
-	//server socket created.
-	int server_socket;
-	server_socket = socket(AF_INET, SOCK_DGRAM, 0);
+	if(listen(probing_socket,1) <0){
+		printf("Error while listening");
+		return -1;
+	}
 
-	//define server address
-	struct sockaddr_in server_address, client_address;
+	client_size = sizeof(client_probing_address);
+	client_probe = accept(probing_socket,(struct sockaddr *)&client_probing_address, &client_size);
 
-	memset(&server_address,0,sizeof(server_address));
-	memset(&client_address,0, sizeof(client_address));
+	if(client_probe < 0){
+		printf("cannot accept address");
+		return -1;
+	}
 
-	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(8765);
-	server_address.sin_addr.s_addr=INADDR_ANY;
+	if (recv(client_probe,port_num,sizeof(port_num),0) <0){
+		printf("couldn't receive");	
+		return -1;
+	}
 
-	//bind the socket to the port and ip
-	if(bind(server_socket,(struct sockaddr*) &server_address, sizeof(server_address))<0){
-		printf("Binding failed");
-	};
+	printf("Port Number: %s\n",port_num);
 
-	int len, n;
-	len = sizeof(client_address);
-	recvfrom(server_socket,bytes,sizeof(bytes),MSG_WAITALL,(struct sockaddr*) &client_address,len);
-	printf("server received!:");
-	//close the socket
-	close(server_socket);
+	close(probing_socket);
+	close(client_probe);
+
+	// //server socket created.
+	// int server_socket;
+	// server_socket = socket(AF_INET, SOCK_DGRAM, 0);
+
+	// //define server address
+	// struct sockaddr_in server_address, client_address;
+
+	// memset(&server_address,0,sizeof(server_address));
+	// memset(&client_address,0, sizeof(client_address));
+
+	// server_address.sin_family = AF_INET;
+	// server_address.sin_port = htons(8765);
+	// server_address.sin_addr.s_addr=INADDR_ANY;
+
+	// //bind the socket to the port and ip
+	// if(bind(server_socket,(struct sockaddr*) &server_address, sizeof(server_address))<0){
+	// 	printf("Binding failed");
+	// };
+
+	// int len, n;
+	// len = sizeof(client_address);
+	// recvfrom(server_socket,bytes,sizeof(bytes),MSG_WAITALL,(struct sockaddr*) &client_address,len);
+	// printf("server received!:");
+	// //close the socket
+	// close(server_socket);
 	return 0;
 
 }
